6c241287cffce2b5fe3e6486f09e53faad5b096c
Chaitalicode <69461926+Chaitalicode@users.noreply.github.com>
2021-01-19 16:34:12 +0530

    Add files via upload

---
+++ SmartProject (1)/UI5Module/webapp/controller/View1.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+		"sap/ui/core/mvc/Controller"
+	],
+	/**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("ST.UI5Module.controller.View1", {
+			onInit: function () {   
+                debugger;     
+            //     var oView = this.getView();
+			// oView.bindElement("/Orders('10261')");
+                    //  this.getView().bindElement("/Orders('10248')");
+			}
+		});
+	});
---
+++ SmartProject (1)/UI5Module/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ST/UI5Module/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
